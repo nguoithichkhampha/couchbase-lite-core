ac2b52d52c58acdc7dd7e3c536b2163388b1df32
tham_ha <tham@emotiv.com>
2018-03-07 15:29:43 +0700

    change couchbase build config for integrating into qbs build

--- build_cmake/scripts/build_windows.bat
+++ build_cmake/scripts/build_windows.bat
@@ -3,16 +3,16 @@ mkdir x86
 mkdir x64
 mkdir arm
 cd x86
-"C:\Program Files\CMake\bin\cmake.exe" ..\..
-msbuild LiteCore.sln /p:Configuration=RelWithDebInfo
+"cmake.exe" ../.. -DSTATIC_OUTPUT_DIR=%1
+cmake --build ./ 
 
-cd ..
-cd x64
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 14 2015 Win64" ..\..
-msbuild LiteCore.sln /p:Configuration=RelWithDebInfo /t:LiteCore
+#cd ..
+#cd x64
+#"cmake.exe" -G "Visual Studio 14 2015 Win64" ..\..
+#msbuild LiteCore.sln /p:Configuration=RelWithDebInfo /t:LiteCore
 
-cd ..
-cd arm
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 14 2015 ARM" ..\..
-msbuild LiteCore.sln /p:Configuration=RelWithDebInfo /t:LiteCore
+#cd ..
+#cd arm
+#"cmake.exe" -G "Visual Studio 14 2015 ARM" ..\..
+#msbuild LiteCore.sln /p:Configuration=RelWithDebInfo /t:LiteCore
 popd
